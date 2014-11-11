7c8b4c66b166c8fff0b26969cee99f60af7dc46b
Arletaxx <arletawasiuk@wp.pl>
2014-11-11 21:01:23 +0100

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lab702-03
+ */
+public class hello {
+       public static void main(String[] args) {
+        int a;
+        a=5;
+        
+        System.out.println("Witam Serdecznie");
+        System.out.println("Liczba a wynosi:"+ a);
+        
+    }
+    }
+
