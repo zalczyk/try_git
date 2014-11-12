649250f731e2b7913881062f8fcba0b46dc69325
mstachow <squall_gd@live.com>
2014-11-12 13:11:18 +0100

    Exercise 6
    Created simple class for exercise 6
    
    Author: Michal Stachowski(squall_gd@live.com)

---
+++ src/exercise_6_mstachowski.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Michał Stachowski
+ * Student of IT & Econometrics at M&FF - GUT
+ * Student of IT at IF - PJATK
+ * Software Engineer at Intel Corporation
+ * email address: michal.stachowski@intel.com
+ * email address: squall_gd@live.com
+ * email address: stachu93@gmail.com
+ * email address: micstach2@student.pg.gda.pl
+ */
+public class exercise_6_mstachowski {
+    public void exercise_6_method(){
+        System.out.println("Some text");
+    }
+}
